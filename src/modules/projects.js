9a87fa91071382e13c02d4eef7669b9d59a720ed
MykolasMi <mykolas.miskinisss@gmail.com>
2023-01-16 21:04:57 +0200

    add functions to allow new tasks be added to different project arrays

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -2,9 +2,33 @@ import { addProject } from "./dom";
 import { showProjects } from "./dom";
 import { loadProjectsForm } from "./dom";
 
-let personal = [];
-let projects = [];
-projects.push(personal);
+class project {
+    constructor(name) {
+        this.name = name;
+        this.length = 0;
+        this.data = [];
+    }
+
+    addTaskToProject(newTask) {
+        this.data.push(newTask)
+    }
+}
+
+
+    let personal = new project('personal')
+    let projects = [];
+    projects.push(personal);
+
+    /*console.log(personal);
+    let task = 'lol'
+    personal.data.push(task);
+    personal.addTaskToProject(task);
+    console.log(personal); //VEIKIA CLASS FUNKCIJA PRIDETI TASKUS
+    console.log('...')
+    console.log(personal.data);*/ //VEIKIA ARRAY CLASS REIKIA DB PUSHINT TASKUS
+
+
+
 
 
 function windowListener () {
@@ -26,7 +50,7 @@ function addProjects () {
     const projectName = document.querySelector('#projectName')
 
     let newProject = new project(projectName);
-    projects.push(newProject);
+    projects.push(newProject); //perkelt sita i class
     showProjects(newProject);
     loadProjectsForm(newProject);
 
@@ -35,12 +59,32 @@ function addProjects () {
     console.log('blet');
 }
 
-class project {
-    constructor(name) {
-        this.name = name.value; //PADARYTI IS SITO KAD KURTU ARRAY
-        this.length=0;
-        this.data = [];
+function associateInputWithArray(projectArray) {
+    console.log('..')
+    console.log(projects)
+    console.log(projects[0].name)
+    console.log(projectArray)
+    for (let i=0; i<projects.length; i++) {
+        if (projects[i].name == projectArray) {
+            console.log('zjbs')
+            //let num = [i]
+            return projects[i];
+        }
+        else {
+            console.log('continue')
+            continue
+        }
     }
 }
 
-export { windowListener, submitListenerPr };
+function addTaskToProjetArray (newTask, projectArray) {
+    //associateInputWithArray(projectArray);
+    //console.log(num)
+    associateInputWithArray(projectArray).addTaskToProject(newTask);
+    console.log(personal);
+    //console.log(newTask)
+    //console.log(projectArray.value)
+    //projectArray.value.addTaskToProject(newTask);
+}
+
+export { windowListener, submitListenerPr, addTaskToProjetArray};
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,4 +1,5 @@
 import { addNew, showTask } from "./dom";
+import { addTaskToProjetArray } from "./projects";
 
 function submitListener () {
     const form = document.querySelector('#form');
@@ -25,18 +26,31 @@ class task {
 }
 
 function addTask () {
+    //let personal = [];
     const taskName = document.querySelector('#taskName');
     const taskLocation = document.querySelector('#location');
 
     let newTask = new task(taskName, taskLocation);
 
-    console.log(taskLocation.value);
-    let selectedArray = taskLocation.value;
+    addTaskToProjetArray(newTask, taskLocation.value);
 
-    selectedArray.push(task); //padaryti kad pushintu i tinkama array projekta
+    //personal.addTaskToProject(newTask);
+
+    /*console.log(taskLocation.value);
+    let selectedArray = taskLocation.value; //t.value.push is not a function?
+    console.log(selectedArray);
+    console.log(personal);
+    console.log(newTask)
+    console.log(newProject) //PADARYTI IS CLASS FUNCTIONO BET PERSONAL DABAR NET NE CLASS
+
+    //padaryti kad cia selectintu is esamu arrays
+
+    selectedArray.push(newTask); //padaryti kad pushintu i tinkama array projekta*/
+
+
 
     showTask(newTask.name);
-    console.log('works');
+    console.log('addTask() works');
 }
 
 
